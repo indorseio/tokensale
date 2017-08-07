4799952faf385d776d556ee397147163a92c545d
gaurangtorvekar <gaurangtorvekar@gmail.com>
2017-08-07 16:22:08 +0800

    Added IND future vesting and inflation vesting

--- contracts/INDInflationVesting.sol
+++ contracts/INDInflationVesting.sol
@@ -89,35 +89,19 @@ contract StandardToken is ERC20, SafeMath {
 
 }
 
-contract INDvesting {
+contract INDInflationVesting {
   mapping (address => uint256) allocations;
   uint256 public unlockDate;
+  uint256 public entitled;
   address public IND;
   uint256 public constant exponent = 10**18;
 
-  function INDvesting(address _IND) {
+  function INDInflationVesting(address _IND) {
     IND = _IND;
-    unlockDate = now + 240 days;
-
+    unlockDate = now + 270 days;
+    
     // Advisors
-    allocations[0xe8C67375D802c9Ae9583df38492Ff3be49e8Ca89] = 100000;
-    allocations[0x3DFb8A970e8d11B4002b2bc98d5a09b09Da3482c] = 100000;
-    allocations[0xC865a2220960585A0D365E8D0d7897d4E3547ae6] = 10000;
-    allocations[0x0DC77D48f290aCaC0e831c835714Ae45e65Ac3d8] = 150000;
-    allocations[0x9628dB0f162665C34BFC0655D55c6B637552B9ec] = 50000;
-    allocations[0x89B7c9c2D529284F9E942389D0894EEadF34f037] = 150000;
-    allocations[0xee4918fbd8Cd49a46B66488C523c3C24d9426270] = 100000;
-    allocations[0xc8A1DAb586DEe8a30Cb88C87b8A3614E0a391fC5] = 100000;
-    allocations[0x0ed1374A831744aF48174a890BbA5ac333e76717] = 50000;
-    allocations[0x293a0369D58aF2433C3A435A6B5343C5455C4eD4] = 100000;
-    allocations[0xf190f0193b694d9d2bb865a66f0c17cbd8280c71] = 50000;
-    allocations[0xB0D9693eEC83452BD54FA5E0318850cc1B1a4a19] = 150000;
-
-    // Team
-    allocations[0x00e21B56A62ff177331C38A359AE0b316fa432Cc] = 6259469;
-    allocations[0xa6565606564282E2E23a86689d43448F6fc3236E] = 6259469;
-    allocations[0xFaa2480cbCe8FAa7fb706f0f16C9AB33873A1E38] = 3129734;
-    allocations[0x60FA8f4324c8082B6155253C3DFe46728Ef6fa20] = 3129734;
+    allocations[0xe8C67375D802c9Ae9583df38492Ff3be49e8Ca89] = 14670632;
   }
 
   function unlock() external {
@@ -126,5 +110,5 @@ contract INDvesting {
     allocations[msg.sender] = 0;
     require(StandardToken(IND).transfer(msg.sender, entitled * exponent));
   }
-
 }
+
