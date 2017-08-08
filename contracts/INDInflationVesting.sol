abda55837900ac475bf1fb4d5f5e524849863417
gaurangtorvekar <gaurangtorvekar@gmail.com>
2017-08-08 16:28:32 +0800

    Cleaned the code

--- contracts/INDInflationVesting.sol
+++ contracts/INDInflationVesting.sol
@@ -160,32 +160,32 @@ contract IndorseToken is SafeMath, StandardToken, Pausable {
     string public version = "1.0";
 
     // contracts
-    address public indSaleDeposit = 0x0053B91E38B207C97CBff06f48a0f7Ab2Dd81449;      // deposit address for Indorse reserve
-    address public indSeedDeposit = 0x0083fdFB328fC8D07E2a7933e3013e181F9798Ad;    // deposit address for Indorse Future reserve
-    address public indPresaleDeposit = 0x007AB99FBf023Cb41b50AE7D24621729295EdBFA;   // deposit address for Indorse Future reserve
-    address public indVestingDeposit = 0x0011349f715cf59F75F0A00185e7B1c36f55C3ab; // deposit address for Indorse Inflation pool
-    address public indCommunityDeposit = 0x0097ec8840E682d058b24E6e19E68358d97A6E5C; // deposit address for Indorse Inflation pool
-    address public indFutureDeposit = 0x00d1bCbCDE9Ca431f6dd92077dFaE98f94e446e4; // deposit address for Indorse Inflation pool
-    address public indInflationDeposit = 0x00D31206E625F1f30039d1Fa472303E71317870A; // deposit address for Indorse Inflation pool
+    address public indSaleDeposit = 0x0053B91E38B207C97CBff06f48a0f7Ab2Dd81449;     
+    address public indSeedDeposit = 0x0083fdFB328fC8D07E2a7933e3013e181F9798Ad;    
+    address public indPresaleDeposit = 0x007AB99FBf023Cb41b50AE7D24621729295EdBFA; 
+    address public indVestingDeposit = 0x0011349f715cf59F75F0A00185e7B1c36f55C3ab; 
+    address public indCommunityDeposit = 0x0097ec8840E682d058b24E6e19E68358d97A6E5C;
+    address public indFutureDeposit = 0x00d1bCbCDE9Ca431f6dd92077dFaE98f94e446e4; 
+    address public indInflationDeposit = 0x00D31206E625F1f30039d1Fa472303E71317870A;
     
-    uint256 public constant indSale = 31603785 * 10**decimals;   // 29 million IND reserved for Indorse use
+    uint256 public constant indSale = 31603785 * 10**decimals;  
     uint256 public constant indSeed = 3566341 * 10**decimals; 
-    uint256 public constant indPreSale = 22995270 * 10**decimals;  // 69.2 million IND for future token sale
-    uint256 public constant indVesting  = 28079514 * 10**decimals;  // 69.2 million IND for future token sale
-    uint256 public constant indCommunity  = 10919811 * 10**decimals;  // 69.2 million IND for future token sale
-    uint256 public constant indFuture  = 58832579 * 10**decimals;  // 69.2 million IND for future token sale
-    uint256 public constant indInflation  = 14624747 * 10**decimals;  // 69.2 million IND for future token sale
+    uint256 public constant indPreSale = 22995270 * 10**decimals;
+    uint256 public constant indVesting  = 28079514 * 10**decimals;
+    uint256 public constant indCommunity  = 10919811 * 10**decimals;
+    uint256 public constant indFuture  = 58832579 * 10**decimals;  
+    uint256 public constant indInflation  = 14624747 * 10**decimals;
    
     // constructor
     function IndorseToken()
     {
-      balances[indSaleDeposit]    = indSale;    // Deposit IND share
-      balances[indSeedDeposit]  = indSeed;  // Deposit IND share
-      balances[indPresaleDeposit] = indPreSale;    // Deposit IND future share
-      balances[indVestingDeposit] = indVesting;    // Deposit IND future share
-      balances[indCommunityDeposit] = indCommunity;    // Deposit IND future share
-      balances[indFutureDeposit] = indFuture;    // Deposit IND future share
-      balances[indInflationDeposit] = indInflation; // Deposit for inflation
+      balances[indSaleDeposit]    = indSale; 
+      balances[indSeedDeposit]  = indSeed;  
+      balances[indPresaleDeposit] = indPreSale;
+      balances[indVestingDeposit] = indVesting;
+      balances[indCommunityDeposit] = indCommunity;
+      balances[indFutureDeposit] = indFuture;    
+      balances[indInflationDeposit] = indInflation;
 
       totalSupply = indSale + indSeed + indPreSale + indVesting + indCommunity + indFuture + indInflation;
 
@@ -214,8 +214,7 @@ contract INDInflationVesting {
   address public IND;
   uint256 public constant exponent = 10**18;
 
-  function INDInflationVesting(address _IND) {
-    IND = _IND;
+  function INDInflationVesting() {
     unlockDate = now + 270 days;
     
     allocations[0x00D31206E625F1f30039d1Fa472303E71317870A] = 14624747;
--- contracts/IndorseToken.sol
+++ contracts/IndorseToken.sol
@@ -3,11 +3,6 @@ import "./StandardToken.sol";
 import "./SafeMath.sol";
 import "./Pausable.sol";
 
-// note introduced onlyPayloadSize in StandardToken.sol to protect against short address attacks
-// Then Deploy IndorseToken and SCRToken
-// Then deploy Sale Contract
-// Then, using indFundDeposit account call approve(saleContract,<amount of offering>)
-
 contract IndorseToken is SafeMath, StandardToken, Pausable {
     // metadata
     string public constant name = "Indorse Token";
@@ -16,32 +11,32 @@ contract IndorseToken is SafeMath, StandardToken, Pausable {
     string public version = "1.0";
 
     // contracts
-    address public indSaleDeposit = 0x0053B91E38B207C97CBff06f48a0f7Ab2Dd81449;      // deposit address for Indorse reserve
-    address public indSeedDeposit = 0x0083fdFB328fC8D07E2a7933e3013e181F9798Ad;    // deposit address for Indorse Future reserve
-    address public indPresaleDeposit = 0x007AB99FBf023Cb41b50AE7D24621729295EdBFA;   // deposit address for Indorse Future reserve
-    address public indVestingDeposit = 0x0011349f715cf59F75F0A00185e7B1c36f55C3ab; // deposit address for Indorse Inflation pool
-    address public indCommunityDeposit = 0x0097ec8840E682d058b24E6e19E68358d97A6E5C; // deposit address for Indorse Inflation pool
-    address public indFutureDeposit = 0x00d1bCbCDE9Ca431f6dd92077dFaE98f94e446e4; // deposit address for Indorse Inflation pool
-    address public indInflationDeposit = 0x00D31206E625F1f30039d1Fa472303E71317870A; // deposit address for Indorse Inflation pool
+    address public indSaleDeposit        = 0x0053B91E38B207C97CBff06f48a0f7Ab2Dd81449;      // deposit address for Indorse Sale contract
+    address public indSeedDeposit        = 0x0083fdFB328fC8D07E2a7933e3013e181F9798Ad;      // deposit address for Indorse Seed Contributors
+    address public indPresaleDeposit     = 0x007AB99FBf023Cb41b50AE7D24621729295EdBFA;      // deposit address for Indorse Presale Contributors
+    address public indVestingDeposit     = 0x0011349f715cf59F75F0A00185e7B1c36f55C3ab;      // deposit address for Indorse Vesting for team and advisors
+    address public indCommunityDeposit   = 0x0097ec8840E682d058b24E6e19E68358d97A6E5C;      // deposit address for Indorse Marketing, etc
+    address public indFutureDeposit      = 0x00d1bCbCDE9Ca431f6dd92077dFaE98f94e446e4;      // deposit address for Indorse Future token sale
+    address public indInflationDeposit   = 0x00D31206E625F1f30039d1Fa472303E71317870A;      // deposit address for Indorse Inflation pool
     
-    uint256 public constant indSale = 31603785 * 10**decimals;   // 29 million IND reserved for Indorse use
-    uint256 public constant indSeed = 3566341 * 10**decimals; 
-    uint256 public constant indPreSale = 22995270 * 10**decimals;  // 69.2 million IND for future token sale
-    uint256 public constant indVesting  = 28079514 * 10**decimals;  // 69.2 million IND for future token sale
-    uint256 public constant indCommunity  = 10919811 * 10**decimals;  // 69.2 million IND for future token sale
-    uint256 public constant indFuture  = 58832579 * 10**decimals;  // 69.2 million IND for future token sale
-    uint256 public constant indInflation  = 14624747 * 10**decimals;  // 69.2 million IND for future token sale
+    uint256 public constant indSale      = 31603785 * 10**decimals;                         
+    uint256 public constant indSeed      = 3566341  * 10**decimals; 
+    uint256 public constant indPreSale   = 22995270 * 10**decimals;                       
+    uint256 public constant indVesting   = 28079514 * 10**decimals;  
+    uint256 public constant indCommunity = 10919811 * 10**decimals;  
+    uint256 public constant indFuture    = 58832579 * 10**decimals;  
+    uint256 public constant indInflation = 14624747 * 10**decimals;  
    
     // constructor
     function IndorseToken()
     {
-      balances[indSaleDeposit]    = indSale;    // Deposit IND share
-      balances[indSeedDeposit]  = indSeed;  // Deposit IND share
-      balances[indPresaleDeposit] = indPreSale;    // Deposit IND future share
-      balances[indVestingDeposit] = indVesting;    // Deposit IND future share
-      balances[indCommunityDeposit] = indCommunity;    // Deposit IND future share
-      balances[indFutureDeposit] = indFuture;    // Deposit IND future share
-      balances[indInflationDeposit] = indInflation; // Deposit for inflation
+      balances[indSaleDeposit]    = indSale;                                                 // Deposit IND share
+      balances[indSeedDeposit]  = indSeed;                                                   // Deposit IND share
+      balances[indPresaleDeposit] = indPreSale;                                              // Deposit IND future share
+      balances[indVestingDeposit] = indVesting;                                              // Deposit IND future share
+      balances[indCommunityDeposit] = indCommunity;                                          // Deposit IND future share
+      balances[indFutureDeposit] = indFuture;                                                // Deposit IND future share
+      balances[indInflationDeposit] = indInflation;                                          // Deposit for inflation
 
       totalSupply = indSale + indSeed + indPreSale + indVesting + indCommunity + indFuture + indInflation;
 
